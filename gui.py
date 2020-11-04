bef15931505e3c43b1bb9ea6978113c0af1c4af1
Elie Pernet <elie.pernet@orange.fr>
2020-11-04 19:16:17 +0100

    début du refactoring du code et avencement du gui

--- gui.py
+++ gui.py
@@ -1,12 +1,13 @@
 from tkinter import *
-from main import calcul
+from main import calcul, calcHypo
 
 root = Tk()
 root.geometry("500x300")
 
 
 def calculer():
-	print("hello world")
+    hypothenuse = calcHypo(sideTriangle1Entry.get(), sideTriangle2Entry.get(), rightAngleEntry.get())
+    print(hypothenuse)
 
 
 unitLabel = Label(root, text="entrer l'unité des longueur du triangle rectangle: ")
@@ -21,12 +22,25 @@ nameLabel.grid(column=0, row=2)
 nameEntry = Entry(root)
 nameEntry.grid(column=1, row=2)
 
-rightAngleLabel = Label(root, text="Entrer le nom de l'angle droit: ")
+rightAngleLabel = Label(root, text="Entrer le nom du sommet de l'angle droit: ")
 rightAngleLabel.grid(column=0, row=3)
 
 rightAngleEntry = Entry(root)
 rightAngleEntry.grid(column=1, row=3)
 
-calculBtn = Button(root, text="calculer", command=calculer)
-calculBtn.grid(column=0, row=4)
+sideTriangle1Label = Label(root, text="entrer le nom d'un des coté de l'angle droit")
+sideTriangle1Label.grid(column=0, row=4)
+
+sideTriangle1Entry = Entry(root)
+sideTriangle1Entry.grid(column=1, row=4)
+
+sideTriangle2Label = Label(root, text="entrer le nom du deuxieme coté de l'angle droit")
+sideTriangle2Label.grid(column=0, row=5)
+
+sideTriangle2Entry = Entry(root)
+sideTriangle2Entry.grid(column=1, row=5)
+
+btnTest = Button(text="test", command=calculer)
+btnTest.grid(column=0, row=6)
+
 root.mainloop()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 import sys
 
 
-def calcul(unit, name_triangle, right_angle, side_triangle2, side_triangle3, hypothenuse, length_side2, length_side3):
+def calcul(unit, name_triangle, right_angle, side_triangle2, side_triangle3, length_side2, length_side3):
     units = ["km", "hm", "dam", "m", "dm", "cm", "mm"]
 
     if not unit in units:
@@ -14,36 +14,36 @@ def calcul(unit, name_triangle, right_angle, side_triangle2, side_triangle3, hyp
         print("Vous ne devez entrer que des nombres, pas de lettre ni de caractere speciaux")
         sys.exit()
 
-    print(f"le triangle {name_triangle} est rectangle en {right_angle} d'hypothenuse {hypothenuse}")
-    print("déprès le théoreme de phytagore:")
-    print(f"on a: {hypothenuse}² = {side_triangle2}² + {side_triangle3}²")
-    print(f"      {hypothenuse}² = {length_side2}² + {length_side3}²")
-
+    hypothenuse = calcHypo("abc", "b")
     side2_length_squared = int(length_side2) ** 2
     side3_length_squared = int(length_side3) ** 2
 
-    print(f"      {hypothenuse}² = {side2_length_squared} + {side3_length_squared}")
-    print(f"      {hypothenuse}² = {side2_length_squared + side3_length_squared}")
-    print(f"      {hypothenuse}² = √{side2_length_squared + side3_length_squared}")
-    print(f"      {hypothenuse} = {math.sqrt(side2_length_squared + side3_length_squared)}")
-    print(f"      {hypothenuse} = {math.floor(math.sqrt(side2_length_squared + side3_length_squared))}")
+    result = f"le triangle {name_triangle} est rectangle en {right_angle} d'hypothenuse {hypothenuse}\n" \
+             f"d'après le théoreme de phytagore:\n" \
+             f"on a: {hypothenuse}² = {side_triangle2}² + {side_triangle3}²\n" \
+             f"      {hypothenuse}² = {length_side2}² + {length_side3}²\n" \
+             f"      {hypothenuse}² = {side2_length_squared} + {side3_length_squared}\n" \
+             f"      {hypothenuse}² = {side2_length_squared + side3_length_squared}\n" \
+             f"      {hypothenuse}² = √{side2_length_squared + side3_length_squared}\n" \
+             f"      {hypothenuse} = {math.sqrt(side2_length_squared + side3_length_squared)}\n" \
+             f"      {hypothenuse} = {math.floor(math.sqrt(side2_length_squared + side3_length_squared))}\n\n" \
+             f"      L'ypothénuse mesure {math.floor(math.sqrt(side2_length_squared + side3_length_squared))} {unit}"
 
-    print(f"      L'ypothénuse mesure {math.floor(math.sqrt(side2_length_squared + side3_length_squared))} {unit}")
+    print(result)
+    return result
+
+
+def calcHypo(triangle_name, right_angle):
+    triangle = triangle_name
+    angleDroit = right_angle
+
+    eachAngle = [i for i in triangle]
+    eachAngle.remove(angleDroit)
+
+    hypothenuse = "".join(eachAngle)
+    print(hypothenuse)
+    return hypothenuse
 
 
 if __name__ == '__main__':
-    unit = input("entrer l'unité des longueur du triangle")
-    name_triangle = input("entrer le nom du triangle")  # nom du triangle
-
-    right_angle = input("entrer le sommet de l'angle droit")  # sommet de l'angle droit
-
-    side_triangle2 = input("entrer le nom du coté 1")
-    side_triangle3 = input("entrer le nom du coté 2")
-
-    hypothenuse = input("entrer la nom du coté de l'hypothenuse")
-
-    length_side2 = input("entrer la longueur du 2eme coté")
-    length_side3 = input("entrer la longueur du 3eme coté")
-
-    calcul(unit, name_triangle, right_angle, side_triangle2, side_triangle3, hypothenuse, length_side2, length_side3)
-
+    calcul("cm", "abc", "b", "ab", "bc", "20", "15")
