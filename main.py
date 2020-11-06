b59e36d55fa27e8626555c349ef72a9cbb467277
Elie Pernet <elie.pernet@orange.fr>
2020-11-06 21:22:08 +0100

    fonction pour recuperer les deux côtés de l'angle droit avec le nom du triangle et l'angle droit fini

--- main.py
+++ main.py
@@ -3,47 +3,59 @@ import math
 import sys
 
 
-def calcul(unit, name_triangle, right_angle, side_triangle2, side_triangle3, length_side2, length_side3):
+def calcul(unit, name_triangle, right_angle, length_side1, length_side2):
     units = ["km", "hm", "dam", "m", "dm", "cm", "mm"]
 
     if not unit in units:
         print("veuillez n'entrer que une unité de longueur valide")
         sys.exit()
 
-    if not length_side2.isdigit() or not length_side3.isdigit():
+    if not length_side1.isdigit() or not length_side2.isdigit():
         print("Vous ne devez entrer que des nombres, pas de lettre ni de caractere speciaux")
         sys.exit()
 
-    hypothenuse = calcHypo("abc", "b")
-    side2_length_squared = int(length_side2) ** 2
-    side3_length_squared = int(length_side3) ** 2
+    all_sides = findSide(name_triangle, right_angle)
 
-    result = f"le triangle {name_triangle} est rectangle en {right_angle} d'hypothenuse {hypothenuse}\n" \
+    hypothenuse = all_sides[0]
+
+    sideTriangle1 = ''.join(list(all_sides[1]))
+    sideTriangle2 = ''.join(list(all_sides[2]))
+
+    side1_length_squared = int(length_side1) ** 2
+    side2_length_squared = int(length_side2) ** 2
+
+    result = f"le triangle {name_triangle} est rectangle en {right_angle} d'hypothenuse [{hypothenuse}]\n" \
              f"d'après le théoreme de phytagore:\n" \
-             f"on a: {hypothenuse}² = {side_triangle2}² + {side_triangle3}²\n" \
-             f"      {hypothenuse}² = {length_side2}² + {length_side3}²\n" \
-             f"      {hypothenuse}² = {side2_length_squared} + {side3_length_squared}\n" \
-             f"      {hypothenuse}² = {side2_length_squared + side3_length_squared}\n" \
-             f"      {hypothenuse}² = √{side2_length_squared + side3_length_squared}\n" \
-             f"      {hypothenuse} = {math.sqrt(side2_length_squared + side3_length_squared)}\n" \
-             f"      {hypothenuse} = {math.floor(math.sqrt(side2_length_squared + side3_length_squared))}\n\n" \
-             f"      L'ypothénuse mesure {math.floor(math.sqrt(side2_length_squared + side3_length_squared))} {unit}"
+             f"on a: {hypothenuse}² = {sideTriangle1}² + {sideTriangle2}²\n" \
+             f"      {hypothenuse}² = {length_side1}² + {length_side2}²\n" \
+             f"      {hypothenuse}² = {side1_length_squared} + {side2_length_squared}\n" \
+             f"      {hypothenuse}² = {side1_length_squared + side2_length_squared}\n" \
+             f"      {hypothenuse}² = √{side1_length_squared + side2_length_squared}\n" \
+             f"      {hypothenuse} = {math.sqrt(side1_length_squared + side2_length_squared)}\n" \
+             f"      {hypothenuse} = {math.floor(math.sqrt(side1_length_squared + side2_length_squared))}\n\n" \
+             f"      L'ypothénuse mesure {math.floor(math.sqrt(side1_length_squared + side2_length_squared))} {unit}"
 
     print(result)
     return result
 
 
-def calcHypo(triangle_name, right_angle):
-    triangle = triangle_name
+def findSide(name_triangle, right_angle):
+    triangle = name_triangle
     angleDroit = right_angle
 
     eachAngle = [i for i in triangle]
     eachAngle.remove(angleDroit)
 
     hypothenuse = "".join(eachAngle)
-    print(hypothenuse)
-    return hypothenuse
+
+    allSides = [i for i in name_triangle]
+    allSides.remove(right_angle)
+    side1 = allSides[0], right_angle
+    side2 = allSides[1], right_angle
+
+    return hypothenuse, side1, side2
 
 
 if __name__ == '__main__':
-    calcul("cm", "abc", "b", "ab", "bc", "20", "15")
+    calcul("cm", "abc", "b", "15", "17")
+
