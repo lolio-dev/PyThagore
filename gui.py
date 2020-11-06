f2df7f09948778d66e98f60228693a84dc1b2d26
Elie Pernet <elie.pernet@orange.fr>
2020-11-06 22:43:39 +0100

    setup du gui pyside2

--- gui.py
+++ gui.py
@@ -1,2 +1,23 @@
 from main import calcul
+from PySide2 import QtWidgets
 
+
+class App(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Théoreme Pythagore")
+        self.setup_ui()
+        self.setup_connections()
+
+    def setup_ui(self):
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.labelText = QtWidgets.QLabel("hello world")
+        self.main_layout.addWidget(self.labelText)
+
+    def setup_connections(self):
+        pass
+
+app = QtWidgets.QApplication([])
+win = App()
+win.show()
+app.exec_()
